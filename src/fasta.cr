4cea2b895a6070bccfddc5f31d14373fbe24b4d0
Wesley Moxam <wes@4ormat.com>
2015-06-13 21:51:59 -0400

    Converts fasta to Crystal
    Changing data tables from array to hash helped avoid unnecessary type
    casting. String concatenation is slow as well, appending to a array is
    much faster.

--- src/fasta.cr
+++ src/fasta.cr
@@ -1,7 +1,8 @@
 # The Computer Language Benchmarks Game
 # http://benchmarksgame.alioth.debian.org/
-# Contributed by Sokolov Yura
+# Ruby version Contributed by Sokolov Yura
 # Modified by Joseph LaFata
+# Ported to Crystal by Wesley Moxam
 
 $last = 42.0
 IM=139968
@@ -20,60 +21,59 @@ alu =
    "AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC"+
    "AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"
 
-iub = [
-    ["a", 0.27],
-    ["c", 0.12],
-    ["g", 0.12],
-    ["t", 0.27],
+iub = {
+  "a" => 0.27,
+  "c" => 0.12,
+  "g" => 0.12,
+  "t" => 0.27,
+  "B" => 0.02,
+  "D" => 0.02,
+  "H" => 0.02,
+  "K" => 0.02,
+  "M" => 0.02,
+  "N" => 0.02,
+  "R" => 0.02,
+  "S" => 0.02,
+  "V" => 0.02,
+  "W" => 0.02,
+  "Y" => 0.02
+}
 
-    ["B", 0.02],
-    ["D", 0.02],
-    ["H", 0.02],
-    ["K", 0.02],
-    ["M", 0.02],
-    ["N", 0.02],
-    ["R", 0.02],
-    ["S", 0.02],
-    ["V", 0.02],
-    ["W", 0.02],
-    ["Y", 0.02],
-]
-homosapiens = [
-    ["a", 0.3029549426680],
-    ["c", 0.1979883004921],
-    ["g", 0.1975473066391],
-    ["t", 0.3015094502008],
-]
+homosapiens = {
+  "a" => 0.3029549426680,
+  "c" => 0.1979883004921,
+  "g" => 0.1975473066391,
+  "t" => 0.3015094502008
+}
 
 def make_repeat_fasta(id, desc, src, n)
     puts ">#{id} #{desc}"
     l = src.length
     s = src * ((n / l) + 1)
-    s.slice!(n, l)
+    s = s[0, n] + s[l, -1]
     0.step(s.length-1,60) {|x| print s[x,60] , "\n"}
 end
 
 def make_random_fasta(id, desc, table, n)
     puts ">#{id} #{desc}"
-    rand, v = nil,nil
     prob = 0.0
-    table.each{|v| v[1]= (prob += v[1])}
-    output = ""
+    table.each{|key, value| table[key] = (prob += value)}
+    output = [] of String
     n.times do
       rand = gen_random(1.0)
-      table.each do |v|
-	if v[1] > rand then
-	  output << v[0]
-	  break
-	end
+      table.each do |key, value|
+        if value > rand
+          output << key
+          break
+        end
       end
     end
-    0.step(output.length-1,60) {|x| print output[x,60] , "\n"}
+    0.step(output.length-1,60) {|x| print output[x,60].join , "\n"}
 end
 
 
-n = (ARGV[0] or 27).to_i
+n = (ARGV[0] || 27).to_i
 
-make_repeat_fasta('ONE', 'Homo sapiens alu', alu, n*2)
-make_random_fasta('TWO', 'IUB ambiguity codes', iub, n*3)
-make_random_fasta('THREE', 'Homo sapiens frequency', homosapiens, n*5)
+make_repeat_fasta("ONE", "Homo sapiens alu", alu, n*2)
+make_random_fasta("TWO", "IUB ambiguity codes", iub, n*3)
+make_random_fasta("THREE", "Homo sapiens frequency", homosapiens, n*5)
